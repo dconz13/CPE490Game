cc2351e4075f2d25681ed4b8906981add899cef4
Dillon Connolly <connollydillon@gmail.com>
2014-11-18 21:54:20 -0500

    Base Files
    Created the eclipse project as well as the main class where the
    heartbeat will be located.

---
+++ src/cpe490game/MainClass.java
@@ -0,0 +1,64 @@
+package cpe490game;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainClass extends Applet implements Runnable, KeyListener {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		super.start();
+	}
+
+	@Override
+	public void update(Graphics arg0) {
+		// TODO Auto-generated method stub
+		super.update(arg0);
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
