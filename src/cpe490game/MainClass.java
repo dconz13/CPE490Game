47bf18070fa467a9663d67e74047c2fb22b64749
Dillon Connolly <connollydillon@gmail.com>
2014-12-01 15:18:49 -0500

    Figured out graphics
    Graphics display pictures now.
    TODO:
    Fix positioning of graphics
    make them responsive to click
    networking
    winner / loser graphics

--- src/cpe490game/MainClass.java
+++ src/cpe490game/MainClass.java
@@ -1,9 +1,24 @@
 package cpe490game;
 
+/*THE PLAN:
+ * 
+ * clear play again flag
+ * on mouse press change update graphic
+ * on release update graphic
+ * on click set flag that its been decided and set player variable (depending on if you are player 1 or 2)
+ * wait till opposite player data has been sent
+ * clear flag, check to see who won locally on devices
+ * winner increments win count
+ * display winner or loser and play again
+ * if play again is selected set flag and send confirmation, wait for response from opponent
+ * 
+ * */
+
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.net.URL;
@@ -14,6 +29,8 @@ public class MainClass extends Applet implements Runnable, MouseListener{
 	private RockPaperScissors gameStart;
 	private Scanner userInput;
 	private URL base;
+	private Graphics second;
+	private Image image,rock,paper,scissor,winner,loser;
 
 	@Override
 	public void destroy() {
@@ -34,6 +51,11 @@ public class MainClass extends Applet implements Runnable, MouseListener{
 		}catch(Exception e){
 			//TODO: handle exception
 		}
+		
+		//grab images from folder
+		rock = getImage(base,"data/rock.png");
+		paper = getImage(base,"data/paper.png");
+		scissor = getImage(base,"data/scissors.png");
 	}
 
 	@Override
@@ -43,15 +65,43 @@ public class MainClass extends Applet implements Runnable, MouseListener{
 		Thread thread = new Thread(this);
 		thread.start();
 	}
-
+	
 	@Override
-	public void update(Graphics arg0) {
-		// TODO Auto-generated method stub
-		super.update(arg0);
+	public void run() {
+		String player1Choice;
+		String player2Choice;
+		
+		while(true){
+			System.out.println("Player 1 pick: ");
+			player1Choice = userInput.nextLine();
+			System.out.println("Player 2 pick: ");
+			player2Choice = userInput.nextLine();
+			gameStart.setInputVariables(player1Choice.toLowerCase(), player2Choice.toLowerCase());
+			gameStart.update();
+			repaint();
+			try {
+				Thread.sleep(17); // sleep every 17ms = 60fps
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
-	public void repaint(){
-		
+	@Override
+	public void update(Graphics g) {
+		if(image == null){
+			image = createImage(this.getWidth(),this.getHeight());
+			second = image.getGraphics();
+		}
+		paint(second);
+		g.drawImage(image,0,0,this);
+	}
+
+	public void paint(Graphics g){
+		g.drawImage(rock, 300, 100, this);
+		g.drawImage(paper, 100, 100, this);
+		g.drawImage(scissor,400,100,this);
 	}
 	
 	@Override
@@ -74,30 +124,14 @@ public class MainClass extends Applet implements Runnable, MouseListener{
 	     */
 		
 		System.out.println("Mouse pressed");
-		repaint();
+		//repaint();
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		/*TODO: this will set the image back to normal*/
 		System.out.println("Mouse released");
-		repaint();
-		
-	}
-
-	@Override
-	public void run() {
-		String player1Choice;
-		String player2Choice;
-		
-		while(true){
-			System.out.println("Player 1 pick: ");
-			player1Choice = userInput.nextLine();
-			System.out.println("Player 2 pick: ");
-			player2Choice = userInput.nextLine();
-			gameStart.setInputVariables(player1Choice.toLowerCase(), player2Choice.toLowerCase());
-			gameStart.update();
-		}
+		//repaint();
 		
 	}
 
@@ -115,17 +149,3 @@ public class MainClass extends Applet implements Runnable, MouseListener{
 	
 
 }
-
-/*THE PLAN:
- * 
- * clear play again flag
- * on mouse press change update graphic
- * on release update graphic
- * on click set flag that its been decided and set player variable (depending on if you are player 1 or 2)
- * wait till opposite player data has been sent
- * clear flag, check to see who won locally on devices
- * winner increments win count
- * display winner or loser and play again
- * if play again is selected set flag and send confirmation, wait for response from opponent
- * 
- * */
