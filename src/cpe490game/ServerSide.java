24e289113a12b904e3b48f46abc9c505af50ea22
Dillon Connolly <connollydillon@gmail.com>
2014-12-08 22:28:55 -0500

    ServerSide done. needs testing

--- src/cpe490game/ServerSide.java
+++ src/cpe490game/ServerSide.java
@@ -9,11 +9,7 @@ import java.net.Socket;
 
 public class ServerSide {
 	
-	private static final String serverAddress = "localhost";
 	private static final int portNumber = 9001;
-	private Socket player1,player2;
-	private static BufferedReader input;
-	private static PrintWriter output;
 	private static ServerSocket serverSocket;
 				
 	public static void main(String args[]) throws IOException{
@@ -24,79 +20,102 @@ public class ServerSide {
 		try{
 		while(true){
 		GameLoop game = new GameLoop();
-		GameLoop.Player player1 = game.new Player(serverSocket.accept(), 1);
-		GameLoop.Player player2 = game.new Player(serverSocket.accept(), 2);
+		System.out.println("Waiting on player 1...");
+		GameLoop.Player player1;
+		player1 = game.new Player(serverSocket.accept(), 1);
+		System.out.println("Player 1 connected!");
+		System.out.println("Waiting on player 2...");
+		GameLoop.Player player2;
+		player2 = game.new Player(serverSocket.accept(), 2);
+		System.out.println("Player 2 connected!");
+		
 		player1.setOpponent(player2);
 		player2.setOpponent(player1);
 		game.currentPlayer = player1;
+		player1.start();
+		player2.start();
+		
 		}
 		}finally{
 			serverSocket.close();
 		}
 	}
+}
 	
-	class GameLoop{
+class GameLoop{
 		
 		Player currentPlayer;
-		private int checkWinner(String player1Choice, String player2Choice){
-			int result = 0;
-			//player1 wins: result = 0 , player2 wins: result = 1, draw: result = 2
-			
+		String player1Choice,player2Choice;
+		String winner;
+		int count = 0;
+		
+		private void checkWinner(String player1Choice, String player2Choice){			
 			switch(player1Choice){
 			case "rock":
 				if(player2Choice.equals("rock")){
-					result = 2;
+					winner = "Draw";
 					break;
 				}else{
 					if(player2Choice.equals("paper")){
-						result = 1;
+						winner = "Player 2 wins!";
 						break;
 					}
 					else{
-						result = 0;
+						winner = "Player 1 wins!";
 					}
 				}
 				break;
 			case "scissor":
 				if(player2Choice.equals("rock")){
-					result = 1;
+					winner = "Player 2 wins!";
 					break;
 				}else{
 					if(player2Choice.equals("paper")){
-						result = 0;
+						winner = "Player 1 wins!";
 						break;
 					}
 					else{
-						result = 2;
+						winner = "Draw";
 					}
 				}
 				break;
 			case "paper":
 				if(player2Choice.equals("rock")){
-					result = 0;
+					winner = "Player 1 wins!";
 					break;
 				}else{
 					if(player2Choice.equals("paper")){
-						result = 2;
+						winner = "Draw";
 						break;
 					}
 					else{
-						result = 1;
+						winner = "Player 2 wins!";
 					}
 				}
 				break;
 			default:
 				break;
 			}
-			return result;
 		}
-	
+		
+		private void setPlayerChoices(String Choice,Player player){
+			if(player == currentPlayer){
+				player1Choice = Choice;
+				count++;
+			}else{
+				player2Choice = Choice;
+				count++;
+				checkWinner(player1Choice,player2Choice);
+			}
+		}
 		
 	class Player extends Thread{
 		
 		private Socket player;
 		private int playerNumber;
 		private Player opponent;
+		private BufferedReader input;
+		private PrintWriter output;
 		
 		public Player(Socket player, int playerNumber){
 			this.player = player;
@@ -105,7 +124,7 @@ public class ServerSide {
 				//Sends a message to the client to choose. Then closes the message. 
 				input = new BufferedReader(new InputStreamReader(player.getInputStream()));
 				output = new PrintWriter(player.getOutputStream(),true);
-				output.println("Welcome "+ playerNumber);
+				output.println("player " + playerNumber + " has connected");
 				
 			}catch(IOException err){
 				System.out.println("player disconnected: ");
@@ -127,7 +146,11 @@ public class ServerSide {
 					if(playerChoice.toLowerCase().equals("quit")){
 						return;
 					}else{
-						
+						setPlayerChoices(playerChoice, this);
+						if(count==2){
+							output.println(winner);
+							count = 0;
+						}
 					}
 				}
 			}catch(IOException err){
@@ -146,6 +169,3 @@ public class ServerSide {
 	}
 	
 }
-}
-
-
