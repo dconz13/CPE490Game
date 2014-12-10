5da805935186b022dffb69894fc7ca162930859b
Dillon Connolly <connollydillon@gmail.com>
2014-12-10 18:29:49 -0500

    Working ServerSide code

--- src/cpe490game/ServerSide.java
+++ src/cpe490game/ServerSide.java
@@ -8,126 +8,129 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerSide {
-	
+
 	private static final int portNumber = 9001;
 	private static ServerSocket serverSocket;
-				
+
 	public static void main(String args[]) throws IOException{
-	
+
 		System.out.println("Starting game server on port " + portNumber + "...");
 		serverSocket = new ServerSocket(portNumber);
 		int globalPlayerNumber = 1;
-		
+
 		try{
 			/*This while loop creates a new game object after 2 players have joined.
 			 *The two players are Thread objects within that game object. 
 			 *If two more players join it will create a new game object for them. 
 			 */
-		while(true){			
-		GameLoop game = new GameLoop();
-		System.out.println("Waiting on player " + globalPlayerNumber + "...");
-		GameLoop.Player player1 = game.new Player(serverSocket.accept(), 1);
-		System.out.println("Player " + globalPlayerNumber + " connected!");
-		globalPlayerNumber++;
-		System.out.println("Waiting on player " + globalPlayerNumber + "...");
-		GameLoop.Player player2 = game.new Player(serverSocket.accept(), 2);
-		System.out.println("Player " + globalPlayerNumber + " connected!");
-		globalPlayerNumber++;
-		
-		player1.setOpponent(player2);
-		player2.setOpponent(player1);
-		game.currentPlayer = player1;
-		player1.start();
-		player2.start();
-		
-		}
+			while(true){			
+				GameLoop game = new GameLoop();
+				System.out.println("Waiting on player " + globalPlayerNumber + "...");
+				GameLoop.Player player1 = game.new Player(serverSocket.accept(), 1);
+				System.out.println("Player " + globalPlayerNumber + " connected!");
+				globalPlayerNumber++;
+				System.out.println("Waiting on player " + globalPlayerNumber + "...");
+				GameLoop.Player player2 = game.new Player(serverSocket.accept(), 2);
+				System.out.println("Player " + globalPlayerNumber + " connected!");
+				globalPlayerNumber++;
+
+				player1.setOpponent(player2);
+				player2.setOpponent(player1);
+				game.currentPlayer = player1;
+				player1.start();
+				player2.start();
+
+			}
 		}finally{
 			serverSocket.close();
 		}
 	}
 }
-	
+
 class GameLoop{
-		
-		Player currentPlayer;
-		String player1Choice,player2Choice;
-		String winner;
-		int count = 0;
-		
-		private void checkWinner(String player1Choice, String player2Choice){	
-			//Logic for the switch variables responses: 
-			//0 = draw, 1 = player 1 wins, 2 = player 2 wins.
-			switch(player1Choice){
-			case "rock":
-				if(player2Choice.equals("rock")){
-					winner = "0";
-					break;
-				}else{
-					if(player2Choice.equals("paper")){
-						winner = "2";
-						break;
-					}
-					else{
-						winner = "1";
-					}
-				}
+
+	Player currentPlayer;
+	String player1Choice,player2Choice;
+	String winner;
+	int count = 0;
+
+	private void checkWinner(String player1Choice, String player2Choice){	
+		//Logic for the switch variables responses: 
+		//0 = draw, 1 = player 1 wins, 2 = player 2 wins.
+		switch(player1Choice){
+		case "rock":
+			if(player2Choice.equals("rock")){
+				winner = "0";
 				break;
-			case "scissor":
-				if(player2Choice.equals("rock")){
+			}else{
+				if(player2Choice.equals("paper")){
 					winner = "2";
 					break;
-				}else{
-					if(player2Choice.equals("paper")){
-						winner = "1";
-						break;
-					}
-					else{
-						winner = "0";
-					}
 				}
+				else{
+					winner = "1";
+				}
+			}
+			break;
+		case "scissor":
+			if(player2Choice.equals("rock")){
+				winner = "2";
 				break;
-			case "paper":
-				if(player2Choice.equals("rock")){
+			}else{
+				if(player2Choice.equals("paper")){
 					winner = "1";
 					break;
-				}else{
-					if(player2Choice.equals("paper")){
-						winner = "0";
-						break;
-					}
-					else{
-						winner = "2";
-					}
 				}
-				break;
-			default:
-				break;
+				else{
+					winner = "0";
+				}
 			}
-		}
-		
-		private void setPlayerChoices(String Choice,Player player){
-			System.out.println("Setting: "+ Choice + " count: "+count);
-			if(player == currentPlayer){
-				player1Choice = Choice;
-				player.playerChoiceFlag = 1;
-				player.opponent.opponentChoiceFlag = 1;
-				count++;
+			break;
+		case "paper":
+			if(player2Choice.equals("rock")){
+				winner = "1";
+				break;
 			}else{
-				player2Choice = Choice;
-				player.playerChoiceFlag = 1;
-				player.opponent.opponentChoiceFlag = 1;
-				count++;
+				if(player2Choice.equals("paper")){
+					winner = "0";
+					break;
+				}
+				else{
+					winner = "2";
+				}
 			}
-			if(count == 2){
-				checkWinner(player1Choice,player2Choice);
-				player1Choice = player2Choice = null;
-				System.out.println("winner after check: "+ winner);
-				count = 0;
-			}
+			break;
+		default:
+			break;
 		}
-		
+	}
+
+	private void setPlayerChoices(String Choice,Player player){
+		System.out.println("Setting: "+ Choice + ", count: "+count);
+		//if player 1, set flags
+		if(player == currentPlayer){
+			player1Choice = Choice;
+			player.playerChoiceFlag = 1;
+			player.opponent.opponentChoiceFlag = 1;
+			count++;
+			//if player 2, set flags
+		}else{
+			player2Choice = Choice;
+			player.playerChoiceFlag = 1;
+			player.opponent.opponentChoiceFlag = 1;
+			count++;
+		}
+		//after both players have chosen. 
+		if(count == 2){
+			checkWinner(player1Choice,player2Choice);
+			player1Choice = player2Choice = null;
+			System.out.println("winner after check: "+ winner);
+			count = 0;
+		}
+	}
+
 	class Player extends Thread{
-		
+
 		private Socket player;
 		private int playerNumber;
 		private Player opponent;
@@ -135,7 +138,7 @@ class GameLoop{
 		private PrintWriter output;
 		public int playerChoiceFlag = 0;
 		public int opponentChoiceFlag = 0;
-		
+
 		public Player(Socket player, int playerNumber){
 			this.player = player;
 			this.playerNumber = playerNumber;
@@ -143,28 +146,26 @@ class GameLoop{
 				//Sends a message to the client to choose. Then closes the message. 
 				input = new BufferedReader(new InputStreamReader(player.getInputStream()));
 				output = new PrintWriter(player.getOutputStream(),true);
-				
+
 				//Let the client know what player number they are.
 				output.println(playerNumber);
-				
+
 			}catch(IOException err){
 				System.out.println("player disconnected: ");
 				err.printStackTrace();
 			}
 		}
-		
+
 		public void setOpponent(Player Opponent){
 			this.opponent = Opponent; 
 		}
-		
+
 		@Override
 		public void run(){
 			String socketInput;
 			//will only run once both players are connected
 			output.println("Opponent connected.");
 			try{
-				//makes sure socketInput != null
-				
 				while(true){
 					socketInput = input.readLine();
 					System.out.println(playerNumber+ ": " + socketInput);
@@ -173,23 +174,22 @@ class GameLoop{
 					}
 					else{
 						if(playerChoiceFlag != 1){
-						setPlayerChoices(socketInput, this);
-						System.out.println("thread " + playerNumber);
+							setPlayerChoices(socketInput, this);
+							System.out.println("thread " + playerNumber);
 						}
 						if((playerChoiceFlag == 1 )&&(opponentChoiceFlag == 1)){
 							System.out.println("Sending winner to: " + playerNumber);
 							output.println(winner);
+							System.out.println("Sending winner to: " + opponent.playerNumber);
 							opponent.output.println(winner);
 							playerChoiceFlag = 0;
 							opponentChoiceFlag = 0;
 							opponent.playerChoiceFlag = 0;
-							opponent.playerChoiceFlag = 0;
-
-							//System.out.println("winner: " +  winner);
+							opponent.opponentChoiceFlag = 0;
 						}
 					}
 				}
-				
+
 			}catch(IOException err){
 				System.out.println("Player " + playerNumber +" has disconnected.");
 				//err.printStackTrace();
